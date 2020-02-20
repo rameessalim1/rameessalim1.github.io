e9c74603ad4c96aacb7909df7d254277fa599658
Ramees Salim <salim.ramees@gmail.com>
2020-02-21 00:05:31 +0530

    add

---
+++ components/header.php
@@ -0,0 +1,43 @@
+        <header>
+            <div class="header-area ">
+                <div id="sticky-header" class="main-header-area">
+                    <div class="container-fluid">
+                        <div class="row align-items-center">
+                            <div class="col-xl-3 col-lg-2">
+                                <div class="logo">
+                                    <a href="index.html">
+                                        <img src="img/logo.png" alt="">
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="col-xl-6 col-lg-7">
+                                <div class="main-menu  d-none d-lg-block">
+                                    <nav>
+                                        <ul id="navigation">
+                                            <li><a class="active" href="index.php">Home</a></li>
+                                            <li><a href="about.php">About</a>
+                                        </li>
+                                        <li><a href="services.php">Services</a></li>
+                                            <li><a href="gallery.php">Gallery</a></li>
+                                <li><a href="contact.php">Contact</a></li>
+                                <li class="call-now"><a href="tel:1111">Call Now</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-lg-3 d-none d-lg-block">
+                    <div class="social_links">
+                        <ul>
+                            <li><a href="#"> <i class="fa fa-phone"></i> </a></li>
+                            <li><a href="#"> <i class="fa fa-whatsapp"></i> </a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-12">
+                    <div class="mobile_menu d-block d-lg-none"></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</header>
